893ab87681a1f220530176a85c180626c4bfb765
higracehuang <higracehuang@gmail.com>
2024-01-15 16:25:13 -0600

    Initial Commit

---
+++ SimplePhotos/SimplePhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePhotosApp.swift
+//  SimplePhotos
+//
+//  Created by Le Huang on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SimplePhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
