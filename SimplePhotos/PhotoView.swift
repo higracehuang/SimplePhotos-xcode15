b1e054090c6bf6d30f2914c4106be8a44d521137
higracehuang <higracehuang@gmail.com>
2024-01-15 16:50:09 -0600

    Added PhotoView

---
+++ SimplePhotos/PhotoView.swift
@@ -0,0 +1,48 @@
+//
+//  PhotoView.swift
+//  SimplePhotos
+//
+//  Created by Le Huang on 1/15/24.
+//
+
+import SwiftUI
+
+struct PhotoView: View {
+  var photoData: Photo
+  
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      AsyncImage(url: URL(string: photoData.thumbnailUrl)) { image in
+        image
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+      } placeholder: {
+        Color.gray
+      }.frame(width: 200, height: 200)
+      
+      Text(photoData.title)
+        .padding(5)
+        .foregroundColor(.black)
+        .frame(width: 200)
+        .lineLimit(2)
+    }
+  }
+}
+
+#Preview("Short title") {
+  PhotoView(photoData: Photo(
+    albumId: 1,
+    id: 1,
+    title: "short",
+    url: "https://via.placeholder.com/600/92c952",
+    thumbnailUrl: "https://via.placeholder.com/200/92c952"))
+}
+
+#Preview("Long title") {
+  PhotoView(photoData: Photo(
+    albumId: 1,
+    id: 1,
+    title: "officia delectus consequatur vero aut veniam",
+    url: "https://via.placeholder.com/160/56a8c2",
+    thumbnailUrl: "https://via.placeholder.com/150/56a8c2"))
+}
